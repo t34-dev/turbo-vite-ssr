40a13a7a92ae26b1b3c357408b929aa53f5e8013
T34 <t34.dev@gmail.com>
2024-10-25 18:40:28 +0300

    init2

--- api/server.mjs
+++ api/server.mjs
@@ -1,23 +1,29 @@
-import { renderPage } from 'vite-plugin-ssr/server';
-import { createServer } from 'http';
+import { renderPage } from 'vike/server'
 
+// We use JSDoc instead of TypeScript because Vercel seems buggy with /api/**/*.ts files
+
+/**
+ * @param {import('@vercel/node').VercelRequest} req
+ * @param {import('@vercel/node').VercelResponse} res
+ */
 export default async function handler(req, res) {
-    const { method, url, headers } = req;
-    const pageContextInit = {
-        urlOriginal: url,
-        method,
-        headers,
-    };
-    const pageContext = await renderPage(pageContextInit);
-    const { httpResponse } = pageContext;
+    const { url } = req
+    console.log('Request to url:', url)
+    if (url === undefined) throw new Error('req.url is undefined')
+
+    const pageContextInit = { urlOriginal: url }
+    const pageContext = await renderPage(pageContextInit)
+    const { httpResponse } = pageContext
+    console.log('httpResponse', !!httpResponse)
 
     if (!httpResponse) {
-        res.statusCode = 404;
-        res.end();
-        return;
+        res.statusCode = 200
+        res.end()
+        return
     }
 
-    const { body, statusCode, contentType, headers: responseHeaders } = httpResponse;
-    res.writeHead(statusCode, { ...responseHeaders, 'Content-Type': contentType });
-    res.end(body);
+    const { body, statusCode, headers } = httpResponse
+    res.statusCode = statusCode
+    headers.forEach(([name, value]) => res.setHeader(name, value))
+    res.end(body)
 }
